e862e99e3b913aeee42a468afc34e11ede23eb00
ajay <ajaynpr1708@gmail.com>
2023-08-01 22:46:02 +0530

    script

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/workspace/reelmail-backend/
+yum update -y
+npm install
---
+++ scripts/start_pm2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/ec2-user/workspace/reelmail-backend/
+# Assuming your application entry point is `app.js`
+sudo pm2 restart 0
