ff247471cd78d7bca9f7cd60abcedf23878b8d3d
Abhishek Sharma <kumarrajay424@gmail.com>
2023-08-04 02:05:11 +0530

    fix

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
   pingTimeout:60000,
   cors: {
-    origin:'http://18.118.12.12:5000'
+    origin:'*'
   }
 })
 
