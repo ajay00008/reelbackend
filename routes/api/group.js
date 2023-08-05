6c2ade46912d4fdd8f4ac9ec3c830a3e919f9f5c
Abhishek Sharma <kumarrajay424@gmail.com>
2023-08-05 19:25:46 +0530

    fix

--- routes/api/group.js
+++ routes/api/group.js
@@ -3,28 +3,30 @@ const router = express.Router();
 const chatroom = require("../../models/chatroom");
 const upload = require("../../upload/upload");
 const fs = require("fs");
-const {uploadImage} = require('../../upload/uploadImage')
+const { uploadImage } = require("../../upload/uploadImage");
 const { groupValidator } = require("../../utils/validators/groupValidator");
 const { validationResult } = require("express-validator");
 
 router.get("/", async (req, res) => {
-  const userId = req.user.id
+  const userId = req.user.id;
   try {
     const totalGroups = await chatroom.countDocuments({
       $or: [
         { admin: userId }, // User is the admin
         { members: { $in: [userId] } }, // User is in the members array
       ],
-    })
-    const groups = await chatroom.find({
-      $or: [
-        { admin: userId }, // User is the admin
-        { members: { $in: [userId] } }, // User is in the members array
-      ],
-    }).populate("members", "username media _id fcmToken")
-    .populate("admin", "username media _id fcmToken");
+    });
+    const groups = await chatroom
+      .find({
+        $or: [
+          { admin: userId }, // User is the admin
+          { members: { $in: [userId] } }, // User is in the members array
+        ],
+      })
+      .populate("members", "username media _id fcmToken")
+      .populate("admin", "username media _id fcmToken");
 
-    return res.status(200).json({ groups, totalGroups , succes:true });
+    return res.status(200).json({ groups, totalGroups, succes: true });
   } catch (err) {
     console.log(err.message);
     res.status(500).send("Server Error");
@@ -38,7 +40,8 @@ router.get("/:id", async (req, res) => {
     return res.status(422).json({ message: "id is required" });
   }
   try {
-    const group = await chatroom.find({ _id: id })
+    const group = await chatroom
+      .find({ _id: id })
       .populate("members", "username media _id fcmToken")
       .populate("admin", "username media _id fcmToken");
     return res.json({ group, status: 200 });
@@ -50,7 +53,7 @@ router.get("/:id", async (req, res) => {
 
 router.post("/", groupValidator, async (req, res) => {
   const loggedInUserId = req.user.id;
-  const { groupName, members, admin , image} = req.body;
+  const { groupName, members, admin, image } = req.body;
 
   console.log(groupName, members, admin, req.body);
   const errors = validationResult(req);
@@ -60,7 +63,8 @@ router.post("/", groupValidator, async (req, res) => {
       .json({ errors: errors.errors[0].msg, success: false });
   }
   try {
-    let groupImage = "https://www.airscan.org/wp-content/uploads/2023/02/Iconen-website-47-1024x1024.png";
+    let groupImage =
+      "https://www.airscan.org/wp-content/uploads/2023/02/Iconen-website-47-1024x1024.png";
     // const data = await resizeAndUploadImage(image);
     const group = new chatroom({
       groupName,
@@ -78,11 +82,11 @@ router.post("/", groupValidator, async (req, res) => {
 });
 
 router.put("/", async (req, res) => {
-  const { groupId, groupName, members , image } = req.body;
-  console.log(image , groupId)
+  const { groupId, groupName, members, image } = req.body;
+  console.log(image, groupId);
   const loggedInUserId = req.user.id.toString();
-  if(!groupId){
-    return res.status(200).json({message:"groupId is required"})
+  if (!groupId) {
+    return res.status(200).json({ message: "groupId is required" });
   }
 
   try {
@@ -95,16 +99,35 @@ router.put("/", async (req, res) => {
 
     if (loggedInUserId !== admin) {
       return res.status(404).json({
-          message: "only admin have access to update group",
-          success: false,
-        });
+        message: "only admin have access to update group",
+        success: false,
+      });
     }
 
     group.groupName = groupName ? groupName : group.groupName;
-    group.members = members ? members : group.members;
-    group.image =  image ? image : group.image
+    group.image = image ? image : group.image;
+
+    const existingMembers = [];
+    const newMembers = [];
+
+    for (const memberId of members) {
+      const memberExists = group.members.includes(memberId);
+      if (memberExists) {
+        existingMembers.push(memberId);
+      } else {
+        newMembers.push(memberId);
+        group.members.push(memberId);
+      }
+    }
+
     const updateRecords = await group.save();
-    res.status(200).json({group: updateRecords, message: "update successfully", success: true});
+    res.status(200).json({
+      group: updateRecords,
+      existingMembers : existingMembers.length,
+      newMembers : newMembers.length,
+      message: "update successfully",
+      success: true,
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message, success: false });
