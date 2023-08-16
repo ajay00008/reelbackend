db69f397352f65c8542cfe496c440e573aa4c08a
Abhishek Sharma <kumarrajay424@gmail.com>
2023-08-16 23:55:31 +0530

    fixes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("media/image", express.static("image"));
 app.use("media/video", express.static("image"));
 
 app.get("/", (req, res) => {
-  res.send("API Running successfully-pipelinedone BOOMS");
+  res.send("API Running successfully-pipelinedone");
 });
 
 app.get("/media/image/:name", (req, res) => {
