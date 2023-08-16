43cf0c0d4752717744c8d9dc697a49e647d2d85d
Abhishek Sharma <kumarrajay424@gmail.com>
2023-08-16 23:54:52 +0530

    fixes

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/ec2-user/workspace/reelmail-backend/
 # Assuming your application entry point is `app.js`
-# sudo pm2 restart 0
-sudo pm2 restart reelbackend
+sudo pm2 restart 0
