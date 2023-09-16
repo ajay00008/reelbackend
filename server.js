4af0876d95dbad44e6709fdfb242ebccb134af7f
Abhishek Sharma <kumarrajay424@gmail.com>
2023-09-16 23:45:58 +0530

    fix social~

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use("media/image", express.static("image"));
 app.use("media/video", express.static("image"));
 
 app.get("/", (req, res) => {
-  res.send("API Running successfully-pipelinedone");
+  res.send("Reel Social Running successfully-pipelinedone");
 });
 
 app.get("/media/image/:name", (req, res) => {
