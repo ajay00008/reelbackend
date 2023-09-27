55888939972fc15acd7b5848ec149f6591877037
Abhishek Sharma <kumarrajay424@gmail.com>
2023-09-27 22:38:01 +0530

    fix

--- test.js
+++
@@ -1,9 +0,0 @@
-const data = {
-    admin:"12",
-    members :["1","2","3"]
-}
-
-const allmembers = [data.admin , ...data.members]
-const filteredMembers = allmembers.filter(member => member !== "12");
-
-console.log(allmembers ,"all" , filteredMembers)
